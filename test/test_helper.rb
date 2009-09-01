73675769014f830bc3a5225a5e35355982e8baf9
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2009-09-01 14:39:34 +0200

    helpers for template creation

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,15 +11,45 @@ require "test/commands"
 
 class Test::Unit::TestCase
   include Test::Commands
+  include FileUtils
 
   def root(*args)
-    File.join(ROOT, *args)
+    File.expand_path File.join(ROOT, *args)
+  end
+  
+  def tmp_path(*args)
+    root("test", "tmp", *args)
   end
 
   def setup
-    dot_monk = File.join(ROOT, "test", "tmp", ".monk")
-
-    FileUtils.rm(dot_monk) if File.exist?(dot_monk)
+    mkdir_p tmp_path
+    rm_f tmp_path(".monk")
+    create_template "default"
+  end
+  
+  def template_path(name = default)
+    tmp_path("templates", name)
+  end
+  
+  def create_template(name, add = "")
+    dir = template_path(name)
+    mkdir_p dir
+    chdir(dir) do
+      unless File.exist? "name"
+        File.open("name", "w") { |f| f << name }
+        raise RuntimeError, "could not initialize git repo" unless system <<-EOS
+          git init -q  &&
+          git add name 2>/dev/null &&
+          git ci -m "created template" -q
+        EOS
+      end
+    end
+    monk("add #{name} #{dir} #{add}") if add
+    dir
+  end
+  
+  def in_template(name, &block)
+    chdir(create_template(name), &block)
   end
 
   def monk(args = nil)
